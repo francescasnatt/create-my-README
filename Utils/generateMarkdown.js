461c0d10243b2e3f6c892b924a79c4baf8136163
Francesca Snatt <francesca.snatt@outlook.com>
2023-01-31 17:34:31 +0000

    readme generator working

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -1,54 +1,54 @@
-function generateReadme(response){
-    return `
-    # ${response.title}
+function generateMarkdown(response){
+return `
+# ${response.title}
       
     
-      ## Description
-    
-      ${response.description}
-    
-      ## Table of Contents 
-    
-      * [Installation](#installation)
-    
-      * [Usage](#usage)
-      
-      * [Contributing](#contributing)
-    
-      * [Tests](#tests)
-    
-      * [Questions](#questions)
-    
-      ## Installation
-    
-      To install necessary dependencies, run the following command:
-    
-      \`\`\`
-      ${response.install}
-      \`\`\`
-    
-      ## Usage
-    
-      ${response.usage}
-    
-      ## Tests
-    
-      To run tests, run the following command:
-    
-      \`\`\`
-      ${response.tests}
-      \`\`\`
-    
-      ## Licence
-    
-      This project requires the ${response.licence} licence.
-    
-      ## Questions
-    
-      If you have any questions about the repo, open an issue or contact me directly at ${response.contact}. You can find more of my work at [${response.username}](https://github.com/${response.username}/).
-    
-    
-    `
+## Description
+  
+${response.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${response.install}
+\`\`\`
+
+## Usage
+
+${response.usage}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${response.tests}
+\`\`\`
+
+## Licence
+
+This project requires the ${response.licence} licence.
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${response.contact}. You can find more of my work at [${response.username}](https://github.com/${response.username}/).
+
+
+`
     }
     
-    module.exports = generateReadme;
+    module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,64 +1,75 @@
 const fs = require('fs');
+const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./Utils/generateMarkdown');
+// const generateReadme = require('./Utils/generateMarkdown');
 
-function init(){
-inquirer
-  .prompt([
-    {
+
+// array of questions for user
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
       type: 'input',
-      message: 'What is the title of your project?',
-      name: 'title',
+      message: 'Please write a short description of your project.',
+      name: 'description',
     },
     {
-        type: 'input',
-        message: 'Please write a short description of your project.',
-        name: 'description',
-      },
-      {
-        type: 'input',
-        message: 'Which command should be run to install necessary dependencies?',
-        name: 'install',
-      },
-      {
-        type: 'input',
-        message: 'Describe how the user can use the application.',
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: 'Which command should be run to test the application?',
-        name: 'tests',
-      },
-      {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'username',
-      },
-      {
-        type: 'input',
-        message: 'Which email address is best for developers to contact you on?',
-        name: 'contact',
-      },
-      {
-        type: 'input',
-        message: 'Input the link to your GitHub repo.',
-        name: 'repo',
-      },
-      {
-        type: 'list',
-        message: 'What kind of licence should your project have?',
-        name: 'licence',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-      },
-  ])
+      type: 'input',
+      message: 'Which command should be run to install necessary dependencies?',
+      name: 'install',
+    },
+    {
+      type: 'input',
+      message: 'Describe how the user can use the application.',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Which command should be run to test the application?',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'Which email address is best for developers to contact you on?',
+      name: 'contact',
+    },
+    {
+      type: 'input',
+      message: 'Input the link to your GitHub repo.',
+      name: 'repo',
+    },
+    {
+      type: 'list',
+      message: 'What kind of licence should your project have?',
+      name: 'licence',
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+  {err ? console.error(err) : console.log("Success!")});
+}
+
+// function to initialize program
+function init() {
+  inquirer
+  .prompt(questions)
   .then((response) => {
-    const readMe = generateReadme(response);
+    const readMe = generateMarkdown(response);
     writeToFile('README.md', readMe);
     });
 }
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-    {err ? console.error(err) : console.log("Success!")});
-}
+// function call to initialize program
+init();
